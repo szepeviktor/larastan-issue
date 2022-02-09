89c9992bc4505b7150d6407cbad91f76b827f9bc
Viktor Szépe <viktor@szepe.net>
2022-02-09 19:39:50 +0000

    Upgrade to Laravel 9 (#2)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
     /**
@@ -30,7 +33,7 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(function (array $attributes) {
+        return $this->state(function () {
             return [
                 'email_verified_at' => null,
             ];
