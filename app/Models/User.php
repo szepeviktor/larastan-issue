5e938816c4fec05c7705df477e5e53ef1d96a73c
Viktor Szépe <viktor@szepe.net>
2022-02-10 10:45:27 +0000

    Test collect()

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Support\Collection;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function foo() : void
+    {
+        /** @var \Illuminate\Support\Collection<array-key, mixed> $items */
+        $items = collect();
+        \PHPStan\dumpType($items);
+    }
 }
