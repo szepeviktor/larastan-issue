7c01cffb606c696f383a4ba1035508a5699fc87f
Viktor Szépe <viktor@szepe.net>
2022-02-15 13:55:48 +0000

    Update UserFactory.php

--- app/UserFactory.php
+++ app/UserFactory.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
+    public function definition()
+    {
+        return [];
+    }
+
     public function configure(): UserFactory
     {
         return $this->afterCreating(function (\App\Models\User $user) {
