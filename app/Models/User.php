3e8af8b286a64bf341920a3a6d8aee96ea3d67f2
Viktor Szépe <viktor@szepe.net>
2022-11-04 18:50:14 +0000

    UUID database field with deafult field name

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -41,4 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(static function (self $user): void {
+            $user->uuid = Str::uuid();
+        });
+    }
 }
