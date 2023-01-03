e7fb59e96ed9b5cd2d6c3852498a4aaf3c367ee1
Viktor Szépe <viktor@szepe.net>
2023-01-03 21:48:40 +0000

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
      */
     public function unverified()
     {
