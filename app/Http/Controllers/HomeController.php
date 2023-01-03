aacd4470fe771ff6b7658fc4a283fa9e6a09cbdb
Viktor Szépe <viktor@szepe.net>
2023-01-03 21:41:02 +0000

    larastan/issues/1507

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    protected User $user;
+
+    public function bar(): void
+    {
+        $this->user = User::factory()->createOne();
+    }
+}
